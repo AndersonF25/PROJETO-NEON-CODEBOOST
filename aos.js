2647a831bb8c91e3cdfbf5bafa3dfe15925af137
Anderson Ferreira <andersonferreira251139@gmail.com>
2023-12-20 10:12:38 -0300

    att animations

--- aos.js
+++ aos.js
@@ -1,24 +1,17 @@
-function AosConfig() {
-    const width = window.innerWidth;
-  
-    if (width > 1024) {
-        AOS.init({
-            disable:false
-        });
-    }else{
-        AOS.init({
-            disable:true
-        })
-    }
+function checkScreenWidth(mq) {
+  if (mq.matches && window.innerWidth > 1024 && window.innerWidth < 2100) {
+    // Chama a função apenas se a largura da tela estiver dentro da faixa desejada
+    AOS.init({
+      disable: false,
+    });
+  } else {
+    AOS.init({
+      disable: true,
+    });
   }
-  function checkScreenWidth(mq) {
-    if (mq.matches) {
-      // Chama a função apenas se a largura da tela estiver dentro da faixa desejada
-      AosConfig();
-    }
 }
-  var mediaQuery = window.matchMedia(
-    "(max-width: 2100px)"
-  );
+var mediaQuery = window.matchMedia("(max-width: 2100px)");
+checkScreenWidth(mediaQuery);
+window.addEventListener("resize", function () {
   checkScreenWidth(mediaQuery);
-  mediaQuery.addEventListener("resize", checkScreenWidth);
+});
